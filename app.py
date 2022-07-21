23839148f862a476a28b409cbec2069342922c64
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-07-21 19:44:56 +0800

    0

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,Blueprint
-from iteration.routes import app_route
-app=Flask(__name__)
-
-
-app.register_blueprint(app_route)
-
-if __name__=='__main__':
-    app.run(debug=True)
--- iteration/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint, render_template
-app_route=Blueprint('app_route',__name__,static_folder='static',template_folder='templates')
-
-@app_route.route('/')
-def home():
-    return render_template('index.html')
